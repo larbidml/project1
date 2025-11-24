f754b62dd7b472b57cf5b359a3508d9beb519dee
larbidml <larbibox@gmail.com>
2025-11-24 21:21:29 +0100

    aaa

--- php/resource/headerPage.php
+++ php/resource/headerPage.php
@@ -5,7 +5,8 @@
   <link rel="icon" type="image/png" href="../../node_modules/bootstrap-icons/icons/ubuntu.svg" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ubuntu-Server</title>
-
+  
+  <!-- cambio -->
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../../node_modules/bootstrap-icons/font/bootstrap-icons.css">
